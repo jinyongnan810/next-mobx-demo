2643f8a46488e7465f49de88ac8866cc0284c1ec
Yuunan Kin <yuunan.kin@gmail.com>
2021-05-14 15:28:02 +0900

    mobx store

--- src/store.tsx
+++ src/store.tsx
@@ -2,4 +2,29 @@ import { makeAutoObservable } from "mobx";
 import sha256 from "crypto-js/sha256";
 import { createContext, useContext, useEffect, FC } from "react";
 
-export {};
+interface IBlock {
+  hash: string;
+  transactions: string[];
+}
+class BlockChainStore {
+  blocks: IBlock[] = [];
+  transactions: string[] = [];
+  constructor() {
+    makeAutoObservable(this);
+  }
+  addTransaction(message: string) {
+    this.transactions.push(message);
+  }
+}
+
+const StoreContext = createContext<BlockChainStore>(new BlockChainStore());
+const StoreProvider: FC<{ store: BlockChainStore }> = ({ store, children }) => {
+  return (
+    <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
+  );
+};
+const useStore = () => {
+  return useContext(StoreContext);
+};
+
+export { BlockChainStore, StoreProvider, useStore };
