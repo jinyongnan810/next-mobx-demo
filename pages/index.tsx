e8be16263cd86f11b65442561ab154339df7a8ca
Yuunan Kin <yuunan.kin@gmail.com>
2021-05-23 09:02:27 +0900

    render some states

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,29 @@
 import { useState, FC } from "react";
 import { observer } from "mobx-react-lite";
-// import { useStore } from "src/store";
+import { useStore } from "src/store";
 
 const Home: FC = () => {
-  return <main>Home</main>;
+  return (
+    <main>
+      <Transactions />
+    </main>
+  );
+};
+
+const Transactions: FC = () => {
+  const store = useStore();
+  return store.transactions.length > 0 ? (
+    <div>
+      <h2>Pending Transactions</h2>
+      <ul className="pending">
+        {store.transactions.map((t, i) => (
+          <li key={`transaction${i}`}>{t}</li>
+        ))}
+      </ul>
+    </div>
+  ) : (
+    <div>No Transactions</div>
+  );
 };
 
 export default Home;
