686625e5fe2a09d62d7d1c2323c59f552685bd81
Yuunan Kin <yuunan.kin@gmail.com>
2021-05-23 09:10:21 +0900

    change state

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,33 @@
-import { useState, FC } from "react";
+import { useState, FC, useRef } from "react";
 import { observer } from "mobx-react-lite";
 import { useStore } from "src/store";
 
 const Home: FC = () => {
   return (
     <main>
+      <Form />
       <Transactions />
     </main>
   );
 };
+
+const Form: FC = () => {
+  const store = useStore();
+  const messageEl = useRef<HTMLInputElement>(null);
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        store.addTransaction(messageEl.current!.value);
+        messageEl.current!.value = "";
+      }}
+    >
+      <input type="text" ref={messageEl} placeholder="message" required />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
 //wrap observer to the component that need to be rerendered when the state changes
 // re-render will only be taken when the related state is changed
 const Transactions: FC = observer(() => {
